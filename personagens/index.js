3ab234d1c19f492c92cb68cddbc26d77073cf284
Murilo Varoto <muriloalves121@Outlook.com>
2024-04-30 12:08:30 -0300

    update sagas

---
+++ personagens/index.js
@@ -0,0 +1,16 @@
+function imgPersonagem(){ //trocar foto do personagem
+
+
+}
+
+function namePersonagem(){ //trocar nome do personagem
+
+}
+
+function creatorPersonagem(){ //trocar nome do criado do personagem
+
+}
+
+function descPersonagem(){//trocar desc do personagem
+
+}
